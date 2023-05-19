e558f1aa44d9453ef06a5f93330887a71baa33ca
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-05-19 14:53:20 +0700

    UPDATE: add Token Generator

--- src/main/java/com/rohesa/jwt/config/JwtService.java
+++ src/main/java/com/rohesa/jwt/config/JwtService.java
@@ -6,10 +6,15 @@ package com.rohesa.jwt.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 /**
@@ -29,6 +34,24 @@ class JwtService {
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+    
+    public String generatedToken(UserDetails userDetails) {
+        return generatedToken(new HashMap<>(), userDetails);
+    }
+    
+    public String generatedToken(
+            Map<String, Object> extracClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extracClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
     
     private Claims extractAllClaims(String token) {

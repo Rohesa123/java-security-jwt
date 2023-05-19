67d87c7b19322515dfb28b124c2c082958259b24
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-05-19 14:02:02 +0700

    UPDATE: add JwtService for filter token

--- src/main/java/com/rohesa/jwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/rohesa/jwt/config/JwtAuthenticationFilter.java
@@ -22,6 +22,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+    
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
@@ -31,12 +33,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request,response);
             return;
         }
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
---
+++ src/main/java/com/rohesa/jwt/config/JwtService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rohesa.jwt.config;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Rohesa
+ */
+
+@Service
+class JwtService {
+
+    String extractUsername(String token) {
+        return null;
+    }
+    
+}

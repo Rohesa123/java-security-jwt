c8d5ca1b854952d099f49a7af798d4e1549aa4c7
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-05-19 16:00:57 +0700

    UPDATE: add application config

---
+++ src/main/java/com/rohesa/jwt/config/ApplicationConfig.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rohesa.jwt.config;
+
+import com.rohesa.jwt.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ *
+ * @author Rohesa
+ */
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    
+    private final UserRepository userRepository;
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}

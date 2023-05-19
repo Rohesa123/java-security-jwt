601e6588c212b0b43bbc4d066c10979ad76df845
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-05-19 14:31:47 +0700

    ADD: add Encryption Key

--- src/main/java/com/rohesa/jwt/config/JwtService.java
+++ src/main/java/com/rohesa/jwt/config/JwtService.java
@@ -4,6 +4,11 @@
  */
 package com.rohesa.jwt.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,9 +18,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 class JwtService {
+    
+    private static final String SECRET_KEY = "655368566D5971337436773979244226452948404D635166546A576E5A723475";
 
-    String extractUsername(String token) {
+    public String extractUsername(String token) {
         return null;
     }
     
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }

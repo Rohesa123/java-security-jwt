9ba69fbb8ee9bfe1a4f24a5ae1d77683482ead16
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-05-22 09:48:47 +0700

    UPDATE: Auth Response

--- src/main/java/com/rohesa/jwt/auth/AuthenticationService.java
+++ src/main/java/com/rohesa/jwt/auth/AuthenticationService.java
@@ -8,6 +8,8 @@ import com.rohesa.jwt.user.User;
 import com.rohesa.jwt.user.UserRepository;
 import com.rohesa.jwt.config.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
     
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -39,6 +42,17 @@ public class AuthenticationService {
     }
     
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generatedToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }

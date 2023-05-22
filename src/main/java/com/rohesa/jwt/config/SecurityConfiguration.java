03834db70014418482ac52308396aebee5a369cb
Rohesa123 <rohesasidiqpermana05@gmail.com>
2023-05-22 09:54:36 +0700

    ADD: add new endpoint

--- src/main/java/com/rohesa/jwt/config/SecurityConfiguration.java
+++ src/main/java/com/rohesa/jwt/config/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
